3dcfa7db8ee410d20568acf1e79cc6edb8a65003
Marshmallow1119 <tomasbras@ua.pt>
2026-05-16 14:48:23 +0100

    add hais algorithm with 4 modes

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -115,6 +115,7 @@ static std::vector<Compressor> make_compressors(const std::string& /*tmp*/) {
         // --- our compressors ---
         { "rais",    "../rais/compress {src} {dst} 2>/dev/null",   "../rais/decompress {src} {dst}", ".rais" },
         { "astra",   "../astra/compress {src} {dst} 2>/dev/null",  "../astra/decompress {src} {dst}", ".astr" },
+        { "hais",    "../hais/compress {src} {dst} 2>/dev/null",   "../hais/decompress {src} {dst}", ".hais" },
     };
 }
 
